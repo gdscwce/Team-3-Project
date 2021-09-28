37e9d145abb3134b88fc67adb5ee40c5fc59c582
Saurabh Nakade <saurabh@Saurabhs-MacBook-Air.local>
2021-09-29 01:32:27 +0530

    Home and NotesList frontend

--- App.js
+++ App.js
@@ -1,18 +1,41 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Home from "./screens/login";
-// import Home from "./screens/signup";
-// import Home from "./screens/lists";
-// import Home from "./screens/createNote";
-// import Home from "./screens/notes";
-// import Home from "./screens/editNotes";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Home from "./screens/Home";
+import ToDoList from "./screens/ToDoList";
+import { title } from "process";
 
-
+const Stack = createStackNavigator();
 
 export default function App() {
-  return(
-    <View>
-      <Home />
-    </View>   
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="NotesApp" component={Home} />
+        <Stack.Screen
+          name="ToDoList"
+          component={ToDoList}
+          options={({ route }) => {
+            return {
+              title: route.params.title,
+              headerStyle: {
+                backgroundColor: route.params.color,
+              },
+              headerTintColor: "white",
+            };
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
